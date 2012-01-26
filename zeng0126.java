7de5e42c23bce0382b8745cc13221b8d3e8062c8
bcc32 <bcc32@anapnea.net>
2012-01-26 08:59:16 -0500

    Jan 26
    variables, concatenation, and operator overload issues

---
+++ zeng0126.java
@@ -0,0 +1,13 @@
+// Aaron Zeng 201200126
+
+public class zeng0126
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Now is the time " );
+        System.out.println( "for all good computers " );
+        int number = ( 9 + 4 ) * 2;
+        // System.out.println( "Our number is " + number + 4 );
+        System.out.println( "Our number is " + ( number + 4 ) );
+    }
+}
