b20f48fbb117d4443e26eec3e28a547fe48d5186
bcc32 <bcc32@anapnea.net>
2012-03-27 13:31:12 -0400

    revise chapter 05 exercise 21
    too awkward; for loop indentation is understood
    must enumerate the triplets

--- c5n21.java
+++ c5n21.java
@@ -5,13 +5,13 @@ public class c5n21
 {
     public static void main( String[] args )
     {
-        System.out.println( "side1\tside2\thypotenuse" );
+        int num = 1;
+        System.out.println( "#\tside1\tside2\thypotenuse" );
         for ( int hypotenuse = 1; hypotenuse <= 500; hypotenuse++ )
-            for ( int side2 = 1; side2 < hypotenuse; side2++ )
-                for ( int side1 = 1; side1 < side2; side1++ )
-                    if ( hypotenuse * hypotenuse == side2 * side2 +
-                        side1 * side1 )
-                        System.out.printf( "%d\t%d\t%d\n", side1, side2,
-                            hypotenuse );
+        for ( int side2 = 1; side2 < hypotenuse; side2++ )
+        for ( int side1 = 1; side1 < side2; side1++ )
+            if ( hypotenuse * hypotenuse == side2 * side2 + side1 * side1 )
+                System.out.printf( "%d\t%d\t%d\t%d\n", num++, side1, side2,
+                    hypotenuse );
     }
 }
