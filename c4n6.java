ac55140f27f7c45adf4cbfb71d62c70e53815452
bcc32 <bcc32@blinkenshell.org>
2012-04-13 00:10:18 -0400

    remove tabs...

--- c4n6.java
+++ c4n6.java
@@ -8,9 +8,9 @@ public class c4n6
 {
     public static void main( String[] args )
     {
-    	int sum = 0, x = 1;
-    	while ( x <= 10 )
-    		sum += x++;
-    	System.out.printf( "The sum of the integers 1..10 is: %s\n", sum );
+        int sum = 0, x = 1;
+        while ( x <= 10 )
+            sum += x++;
+        System.out.printf( "The sum of the integers 1..10 is: %s\n", sum );
     }
 }
