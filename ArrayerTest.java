92c2ab152dbd9189c9247843590cb00ce411421f
bcc32 <bcc32@blinkenshell.org>
2012-04-26 20:54:50 -0400

    new ArrayerTest

--- ArrayerTest.java
+++ ArrayerTest.java
@@ -9,15 +9,11 @@ public class ArrayerTest
     public static void main( String[] args )
     {
         Scanner input = new Scanner( System.in );
-        Arrayer arr = new Arrayer( 10 );
-        System.out.print( "Enter ten integers: " );
-        for ( int i = 0; i < 10; i++ )
-            arr.setVal(i, input.nextInt() );
+        Arrayer arr = new Arrayer( 1000 );
+        arr.randArr();
         arr.printArr();
         System.out.printf( "Minimum: %d\n", arr.min() );
         System.out.printf( "Maximum: %d\n", arr.max() );
-        arr.randArr();
-        arr.printArr();
         System.out.printf( "Sum: %d\n", arr.sum() );
         System.out.println( "Average: " + arr.average() );
     }
